684ab7c04f5b878ab583d3365eaed6fc2bf0f959
Gael <g.ibarraas@gmail.com>
2025-07-16 22:31:01 -0600

    Initial Commit

---
+++ gael-shop/gael_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  gael_shopApp.swift
+//  gael-shop
+//
+//  Created by Ibarra on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct gael_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gael-shopTests/gael_shopTests.swift
@@ -0,0 +1,17 @@
+//
+//  gael_shopTests.swift
+//  gael-shopTests
+//
+//  Created by Ibarra on 16/07/25.
+//
+
+import Testing
+@testable import gael_shop
+
+struct gael_shopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
