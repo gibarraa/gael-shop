6ef5f279b55d6277413ade6163a7650096b43ce4
Gael <g.ibarraas@gmail.com>
2025-07-17 14:02:56 -0600

    se agreg o la documentación del navbar

--- gael-shop/NavbarView.swift
+++ gael-shop/NavbarView.swift
@@ -8,86 +8,89 @@
 import SwiftUI
 
 struct NavbarView: View {
+    //variable para el searchbar
     @State private var searchText: String = ""
 
     var body: some View {
-        HStack(spacing: 20) {
+        HStack(spacing: 20) {//hstack para el contenido principal del navbar
            
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .foregroundColor(.gray)
+            HStack {//hstack para el searchbar
+                Image(systemName: "magnifyingglass")//icono de lupa para el searchbar
+                    .foregroundColor(.gray)//color gris
                 
             
-                TextField("", text: $searchText, prompt: Text("Busca tu artículo favorito").foregroundColor(.gray))
-                    .textFieldStyle(PlainTextFieldStyle())
+                TextField("", text: $searchText, prompt: Text("Busca tu artículo favorito").foregroundColor(.gray))//textfield para escribir con placeholder que manda el texto a la variable del searchbar
+                    .textFieldStyle(PlainTextFieldStyle())//formato plano para el textfield
             }
-            .padding(.vertical, 8)
-            .padding(.horizontal, 12)
-            .cornerRadius(8)
+            .padding(.vertical, 8)//padding vertical de 8
+            .padding(.horizontal, 12)//padding horizontal del 12
+            .cornerRadius(8)//bordes redondeados de 8
             .overlay(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                RoundedRectangle(cornerRadius: 8)//borde redondeado para el searchbar
+                    .stroke(Color.gray.opacity(0.3), lineWidth: 1)//borde gris con opacidad para que se vea más minimalista
             )
-            .frame(maxWidth: .infinity)
+            .frame(maxWidth: .infinity)//frame del searchbar
 
             
-            HStack(spacing: 25) {
-                Button(action: {
+            HStack(spacing: 25) {//hstack con spacing del 25 para poner los botones y la foto de perfil
+                Button(action: {// botón del calendario sin accion
                 }) {
-                    Image(systemName: "calendar")
+                    Image(systemName: "calendar")//icono de calendario
                         .font(.title2)
-                        .foregroundColor(.green.opacity(0.7))
+                        .foregroundColor(.green.opacity(0.7))//color verde con opacidad
                 }
-                .buttonStyle(PlainButtonStyle())
+                .buttonStyle(PlainButtonStyle())//estilo de botón plano
 
-                Button(action: {
+                Button(action: {//botón para el correo sin funcion
                   
                 }) {
-                    Image(systemName: "envelope")
+                    Image(systemName: "envelope")//icono del correo
                         .font(.title2)
-                        .foregroundColor(.green.opacity(0.7))
+                        .foregroundColor(.green.opacity(0.7))// color verde con opacidad
                 }
-                .buttonStyle(PlainButtonStyle())
+                .buttonStyle(PlainButtonStyle())//botón con estilo plano
 
                
-                Button(action: {
+                Button(action: {//botón sin acción para las notificaciones
                     
                 }) {
-                    Image(systemName: "bell")
+                    Image(systemName: "bell") //icono de campana para las notificaciones
                         .font(.title2)
-                        .foregroundColor(.green.opacity(0.7))
+                        .foregroundColor(.green.opacity(0.7))// color verde con opacidad
                 }
-                .buttonStyle(PlainButtonStyle())
+                .buttonStyle(PlainButtonStyle())//botón con estilo plano
 
                 
-                Divider()
-                    .frame(height: 25)
-                    .background(Color.gray.opacity(0.5))
+                Divider()// divider para dividir los botones de la foto de perfil
+                    .frame(height: 25)// altura de 25
+                    .background(Color.gray.opacity(0.5))// color gris con opacidad al 59%
 
                
-                Button(action: {
+                Button(action: {//Botón para la foto de perfil
                     
                 }) {
-                    HStack(spacing: 5) {
-                        Image(systemName: "person.circle.fill")
+                    HStack(spacing: 5) {//hstack para la foto de perfil y la flecha de despliegue
+                        Image(systemName: "person.circle.fill")// simulacion de la foto de perfil
                             .font(.system(size: 32))
                             .foregroundColor(.green.opacity(0.8))
                         
-                        Image(systemName: "chevron.down")
-                            .font(.caption) 
+                        Image(systemName: "chevron.down")//botón de despliegue
+                            .font(.caption)
                             .foregroundColor(.gray)
                     }
                 }
-                .buttonStyle(PlainButtonStyle())
+                .buttonStyle(PlainButtonStyle())// estilo de botón plano
             }
         }
-        .padding(.horizontal)
-        .padding(.vertical, 10)
-        .background(Color.white)
-        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+        .padding(.horizontal)//padding horizontal
+        .padding(.vertical, 10)//padding vertical de 10
+        .background(Color.white)//fondo blanco como en todo el proyecto
+        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)//sombra
     }
 }
 
+
+//preview usado para el desarrollo, ignorar
 struct NavbarView_Previews: PreviewProvider {
     static var previews: some View {
         NavbarView()
